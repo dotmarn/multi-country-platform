5d8e61f19a822d86e00eac7d98ddaa7d3bd30def
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-03-01 23:10:13 +0100

    updated enum list

--- hub-service/app/Enums/EventTypesEnum.php
+++ hub-service/app/Enums/EventTypesEnum.php
@@ -8,4 +8,7 @@ enum EventTypesEnum: string
     case EMPLOYEE_UPDATED = 'employee.updated';
     case EMPLOYEE_DELETED = 'employee.deleted';
     case CHECKLIST_UPDATED = 'checklist.updated';
+    case EMPLOYEE_LIST_UPDATED = 'employee.list.updated';
+    case EMPLOYEE_DATA_CHANGED = 'employee.data.changed';
+
 }
