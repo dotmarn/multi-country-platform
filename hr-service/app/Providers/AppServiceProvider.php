061a811743b1579a0caa9ff294d64db3a0348a06
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-03-01 12:27:13 +0100

    registered service class

--- hr-service/app/Providers/AppServiceProvider.php
+++ hr-service/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\RabbitMQService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(RabbitMQService::class);
     }
 
     /**
