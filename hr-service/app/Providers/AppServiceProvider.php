7fbf341d274896dc2ce96db4f1239827a0abb0c6
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-03-02 12:25:33 +0100

    registered response macros

--- hr-service/app/Providers/AppServiceProvider.php
+++ hr-service/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\RabbitMQService;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Response::macro('success', function ($status, $message, $data = [], $meta = null) {
+            return Response::json(['status' => true, 'message' => $message, 'data' => $data, 'meta' => $meta], $status);
+        });
+
+        Response::macro('error', function ($status, $message, $error = []) {
+            return Response::json(['status' => false, 'message' => $message, 'errors' => $error], $status);
+        });
     }
 }
