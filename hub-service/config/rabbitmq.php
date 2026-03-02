b446c0be652bfb19b8d3185345c76eda9ad19a97
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-03-02 11:06:52 +0100

    added config files

---
+++ hub-service/config/rabbitmq.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host' => env('RABBITMQ_HOST', 'localhost'),
+    'port' => env('RABBITMQ_PORT', 5672),
+    'user' => env('RABBITMQ_USER', 'guest'),
+    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+    'vhost' => env('RABBITMQ_VHOST', '/'),
+    'exchange' => env('RABBITMQ_EXCHANGE', 'employee_events'),
+    'queue' => env('RABBITMQ_QUEUE', 'hub_service_queue'),
+];
