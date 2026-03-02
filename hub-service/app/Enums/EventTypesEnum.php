1e53656de6bc1a7c4323657bf5d1e24f90499aa0
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-03-02 12:48:37 +0100

    updated event

--- hub-service/app/Enums/EventTypesEnum.php
+++ hub-service/app/Enums/EventTypesEnum.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum EventTypesEnum: string
 {
-    case EMPLOYEE_CREATED = 'employee.created';
-    case EMPLOYEE_UPDATED = 'employee.updated';
-    case EMPLOYEE_DELETED = 'employee.deleted';
+    case EMPLOYEE_CREATED = 'EmployeeCreated';
+    case EMPLOYEE_UPDATED = 'EmployeeUpdated';
+    case EMPLOYEE_DELETED = 'EmployeeDeleted';
     case CHECKLIST_UPDATED = 'checklist.updated';
     case EMPLOYEE_LIST_UPDATED = 'employee.list.updated';
     case EMPLOYEE_DATA_CHANGED = 'employee.data.changed';
